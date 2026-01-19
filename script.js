fc44ecfe4c49ffc23afe8c137a86d07370354aeb
alexisrojas1999 <107193215+alexisrojas1999@users.noreply.github.com>
2026-01-19 14:42:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateConfig() {
     // Check required fields
     if (!config.valentineName) {
         warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "Vaquera";
+        config.valentineName = "mi Vaquera";
     }
 
     // Validate colors
